4c0fb7c606af8bf8ffe43151174a3ee2a0e14fe4
IlyaTrat <deeo-sama@hotmail.com>
2019-02-24 19:39:51 +0300

    build

---
+++ precache-manifest.74b56afee58786281333c6dfc083f847.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5280a2c0b2d5e5880bdc",
+    "url": "/rss-mentor-dashboard/static/js/runtime~main.5280a2c0.js"
+  },
+  {
+    "revision": "3d03a8c1b74ae566b13a",
+    "url": "/rss-mentor-dashboard/static/js/main.3d03a8c1.chunk.js"
+  },
+  {
+    "revision": "134243148c8e9bc01730",
+    "url": "/rss-mentor-dashboard/static/js/2.13424314.chunk.js"
+  },
+  {
+    "revision": "3d03a8c1b74ae566b13a",
+    "url": "/rss-mentor-dashboard/static/css/main.fd74cbe2.chunk.css"
+  },
+  {
+    "revision": "a4929495023f594ac3d1a59c1db7dcc9",
+    "url": "/rss-mentor-dashboard/index.html"
+  }
+];
